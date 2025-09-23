de74b10ebc89c85587974c0ad7851c5145dfcbc9
yzy <yanziyi290@gmail.com>
2025-09-24 00:09:27 +0100

    上传进度条

--- src/api/endpoints/uploads.ts
+++ src/api/endpoints/uploads.ts
@@ -1,20 +1,27 @@
 import api from '../api';
+
 /**
- * 批量上传文件
+ * 批量上传文件（支持进度回调）
  * @param files 要上传的文件数组
  * @param type  图片类型（如 'products', 'avatars', 'banners', 'photowall'）
+ * @param onProgress 上传进度回调
  * @returns 返回一个包含 {path: string} 对象的数组的 Promise
  */
-export const uploadFiles = (files: File[], type: string) => {
+export const uploadFiles = (files: File[], type: string, onProgress?: (percent: number) => void) => {
   const formData = new FormData();
   files.forEach(file => {
     formData.append('files', file);
   });
 
-  // 带上 type 参数
   return api.post<{ path: string }[]>(`/uploads/${type}`, formData, {
     headers: {
       'Content-Type': 'multipart/form-data',
+    },
+    onUploadProgress: progressEvent => {
+      if (onProgress) {
+        const percent = Math.round((progressEvent.loaded * 100) / (progressEvent.total || 1));
+        onProgress(percent);
+      }
     },
   });
 };
